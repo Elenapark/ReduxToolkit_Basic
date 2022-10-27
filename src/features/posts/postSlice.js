8ae7578da867411c018739c0e4c9838a60fb008e
elenapark <elenability@naver.com>
2022-10-28 02:21:10 +0900

    add : reaction component and reducers

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -7,12 +7,26 @@ const initialState = [
     title: "Learning Redux Toolkit",
     content: "this is redux toolkit contents!",
     date: sub(new Date(), { minutes: 10 }).toISOString(), // convert to a time stamp string
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: "2",
     title: "this is post slice file",
     content: "the more I say slice, the more I want pizza🍕",
     date: sub(new Date(), { minutes: 5 }).toISOString(), // convert to a time stamp string
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -32,14 +46,29 @@ export const postSlice = createSlice({
             content,
             date: new Date().toISOString(),
             userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        // it looks we are mutating state, but immerJS makes it immutable underneath
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postSlice.actions;
+export const { postAdded, reactionAdded } = postSlice.actions;
 export default postSlice.reducer;
