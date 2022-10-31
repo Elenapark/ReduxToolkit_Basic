25775342cde025595b27c2c424598761102e303c
elenapark <elenability@naver.com>
2022-10-31 23:57:14 +0900

    add : add fetchUsers async function and fetch users on the top

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,16 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "Elena Park" },
-  { id: "1", name: "SungJi Park" },
-  { id: "2", name: "HanSook Ko" },
-  { id: "3", name: "SoonIl Park" },
-];
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  try {
+    const response = await axios.get(USERS_URL);
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
+});
+
+const initialState = [];
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
