c5d8ab049dbda0bddac3904b2c6bff4bab7056fe
elenapark <elenability@naver.com>
2022-11-05 00:55:14 +0900

    add : update/delete post createAsyncThunk

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -30,6 +30,34 @@ export const addNewPost = createAsyncThunk(
     }
   }
 );
+export const updatePost = createAsyncThunk(
+  "posts/updatePost",
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
+      return response.data;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
+export const deletePost = createAsyncThunk(
+  "posts/deletePost",
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.delete(`${POSTS_URL}/${id}`);
+      // 통상적인 api응답으로는 삭제한 post의 id를 주지만 jsonplaceholder에서는 다르게 오므로
+      // 아래외 같은 제약조건 추가
+      // return same initialPost so we can grab the id out of it, iniside of our builder case
+      if (response?.status === 200) return initialPost;
+      return `${response?.status} : ${response?.statusText}`;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
 
 export const postSlice = createSlice({
   name: "posts",
@@ -112,6 +140,32 @@ export const postSlice = createSlice({
         };
         console.log("addNewPost:", action.payload);
         state.posts.push(action.payload);
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Update could not complete");
+          console.log(action.payload);
+          return;
+        }
+
+        const { id } = action.payload;
+        // add date manually
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = [...posts, action.payload];
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Delete could not complete");
+          console.log(action.payload);
+          return;
+        }
+
+        const { id } = action.payload;
+        // add date manually
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = posts;
       });
   },
 });
